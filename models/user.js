7a87e6b4633e6eb2f71d877e67cdbf2581557805
Abhisek Chakraborty <abhisekchakraborty8@gmail.com>
2018-11-01 09:32:27 +1100

    User authentication with Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+//by importing passport local mongoose(plm) we no longer need to keep user id and password in the schema since its a part of PLM
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
